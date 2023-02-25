238ea2b6311552f5a9a42237da5d2c27723dc993
chandanduke <chandan.kumar1@citrix.com>
2023-02-25 18:42:39 +0530

    performane optimization

--- BE/index.js
+++ BE/index.js
@@ -17,18 +17,22 @@ const getString = () => {
     const tag1 = Math.random().toString(36).substring(2,7);
     const tag2 = Math.random().toString(36).substring(2,7);
     const tag3 = Math.random().toString(36).substring(2,7);
+    const tag4 = Math.random().toString(36).substring(2,7);
+    const tag5 = Math.random().toString(36).substring(2,7);
+    const tag6 = Math.random().toString(36).substring(2,7);
     const number1 = (Math.random()*1000).toFixed(2);
     const number2 = (Math.random()*1000).toFixed(2);
-    return  {Tag1: tag1, Tag2: tag2,Tag3: tag3, Metric1: number1, Metric2: number2};
+    const number3 = (Math.random()*1000).toFixed(2);
+    return  {Tag1: tag1, Tag2: tag2,Tag3: tag3,Tag4: tag4, Tag5: tag5,Tag6: tag6, Metric1: number1, Metric2: number2, Metric3: number3};
 
 }
-
+let delay = 600;
 const sendData = (connection) => {
     const result = getString();
     setTimeout(() => {
         connection.send(JSON.stringify(result));
         sendData(connection);
-    },1000)
+    },delay+=10)
 
 }
 wsServer.on('request', (request) => {
--- FE/my-app/src/Dashboard.js
+++ FE/my-app/src/Dashboard.js
@@ -5,32 +5,14 @@ import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 const Dashboard = () => {
+  // URL of the node server 
   const url = "ws://127.0.0.1:8000"
   const gridRef = useRef();
   const [rowData, setRowData] = useState([]);
+
+
+  // Websocket connection logic
   const { sendMessage, lastMessage, readyState } = useWebSocket(url);
-  const [columnDefs] = useState([
-    { field: 'Tag1', sortable: true, filter: "agTextColumnFilter" },
-    { field: 'Tag2', sortable: true, filter: "agTextColumnFilter" },
-    { field: 'Tag3', sortable: true, filter: "agTextColumnFilter" },
-    { field: 'Metric1', sortable: true, filter: "agNumberColumnFilter" },
-    { field: 'Metric2', sortable: true, filter: "agNumberColumnFilter" }
-])
-  useEffect(() => {
-    if (lastMessage !== null) {
-        const newRecord = JSON.parse(lastMessage.data);
-        gridRef.current.api.applyTransaction({
-          addIndex: 0,
-          add: [newRecord]
-        })
-        // const updatedRowData = [newRecord, ...rowData];
-        // setRowData(updatedRowData);
-    }
-  }, [lastMessage]);
-
-  useEffect(() => {sendMessage('Hello')},[]);
-  const handleClickSendMessage = useCallback(() => sendMessage('Hello'), []);
-
   const connectionStatus = {
     [ReadyState.CONNECTING]: 'Connecting',
     [ReadyState.OPEN]: 'Open',
@@ -39,11 +21,42 @@ const Dashboard = () => {
     [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
   }[readyState];
 
+  // Columns def
+  const [columnDefs] = useState([
+    { field: 'Tag1', sortable: true, filter: "agTextColumnFilter" },
+    { field: 'Tag2', sortable: true, filter: "agTextColumnFilter" },
+    { field: 'Tag3', sortable: true, filter: "agTextColumnFilter" },
+    { field: 'Tag4', sortable: true, filter: "agTextColumnFilter" },
+    { field: 'Tag5', sortable: true, filter: "agTextColumnFilter" },
+    { field: 'Tag6', sortable: true, filter: "agTextColumnFilter" },
+    { field: 'Metric1', sortable: true, filter: "agNumberColumnFilter" },
+    { field: 'Metric2', sortable: true, filter: "agNumberColumnFilter" },
+    { field: 'Metric3', sortable: true, filter: "agNumberColumnFilter" },
+])
+
+// Adding data to the grid
+  useEffect(() => {
+    if (lastMessage !== null) {
+        const newRecord = JSON.parse(lastMessage.data);
+        gridRef.current.api.applyTransaction({
+          addIndex: 0,
+          add: [newRecord]
+        })
+    }
+  }, [lastMessage]);
+
+  // As soon as user land on the page they should see latest update 
+  useEffect(() => {sendMessage('Hello')},[]);
+  const handleClickSendMessage = useCallback(() => sendMessage('Hello'), []);
+
+// grid 
 
   return (
-      <div className="ag-theme-alpine" style={{width: 1000}}>
+      <div className="ag-theme-alpine">
+        <h1 style = {{ display: "flex", color: "green", justifyContent: "center"  }} >Live Dashboard</h1>
            <AgGridReact ref={gridRef}
                domLayout='autoHeight'
+               pagination={true}
                enableCellChangeFlash = {true}
                rowData={rowData}
                columnDefs={columnDefs}>
